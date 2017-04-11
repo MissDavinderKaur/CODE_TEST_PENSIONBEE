4ba0932ef56a5c8ef7f3b026cb028e090d8cfaa0
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-04-11 20:01:05 +0100

    Tested app in the browser and it works

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const read = require('read-file');
+const marked = require('marked');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const template = read.sync(`${__dirname}/template.html`, 'utf8');
+
+app.get('/:folder', function(req, res) {
+  try {
+    const content = read.sync(`${__dirname}/content/${req.params.folder}/index.md`, 'utf8');
+    const page = template.replace(/{{content}}/, marked(content));
+
+    res.status(200);
+    res.send(page);
+  } catch (e) {
+    res.status(404);
+    res.send();
+  }
+});
 
 app.listen(port, () => {
   console.log(`Application has started on port: ${port}`);
