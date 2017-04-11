5f86e5abc8bb377dd70320a75e3876343ca9e55a
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-04-11 22:08:22 +0100

    Updated error in test - mock should not be returning anything

--- utils/__tests__/program.tests.js
+++ utils/__tests__/program.tests.js
@@ -10,19 +10,7 @@ describe('Tests for Acme website', () => {
     };
     res = {
       status: jest.fn(),
-      send: jest.fn().mockReturnValue(`
-        <!doctype html>
-        <html>
-        <head>
-        <title>Welcome to Acme</title>
-        </head>
-        <body>
-        <h1 id="this-is-the-about-page">This is the About page</h1>
-        <p>Acme Co. is a reputable maker of widgets and is an international brand.</p>
-        <p>Thank you for your interest in our services. Please contact us at enquiries@acme.com.</p>
-
-        </body>
-        </html>`)
+      send: jest.fn()
       };
     });
 
