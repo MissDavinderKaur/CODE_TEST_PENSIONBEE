b7c3e19022388da6c6c793935093dd564e5ec9cb
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-04-11 19:43:08 +0100

    Installed read-file and set up the template constant

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const read = require('read-file');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const template = read.sync(`${__dirname}/template.html`, 'utf8');
 
 app.listen(port, () => {
   console.log(`Application has started on port: ${port}`);
