62619602a7f5a69c2f1d80a9a92a00457b92fe3e
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-04-11 20:45:17 +0100

    Refactored code further (using pathExists) to allow for testing

--- __tests__/tests.js
+++
@@ -1,4 +0,0 @@
-it('Should return the correct HTTP code', () => {
-    createBlog(req, res);
-    expect(res.status).toBeCalledWith(200);
-  });
--- utils/program.js
+++ utils/program.js
@@ -1,23 +1,24 @@
 const read = require('read-file');
 const marked = require('marked');
+const pathExists = require('path-exists')
 
 const Program = {};
 
 Program.template = read.sync(`./template.html`, 'utf8');
 
 Program.functionality = function(req, res) {
-
-  try {
+  pathExists(`./content/${req.params.folder}/index.md`)
+  .then(()=> {
     const content = read.sync(`./content/${req.params.folder}/index.md`, 'utf8');
     const page = Program.template.replace(/{{content}}/, marked(content));
 
     res.status(200);
     res.send(page);
-  } catch (e) {
+  })
+  .catch(() => {
     res.status(404);
     res.send();
-  }
+  });
 };
-
 
 module.exports = Program;
